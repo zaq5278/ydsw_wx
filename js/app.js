74c52932851f0ce1d99465b7708f633bb7434d95
zaq5278 <527895805@qq.com>
2016-05-30 14:40:00 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -22,8 +22,7 @@ angular.module('myApp', ['ionic','myApp.mainController'])
         url: '/login',
         views: {
           'index.main': {     //这里必须有一个父子关系的路由才能管控很多路由  index.main就相当于一个路由域
-            templateUrl: "login.html",
-            controller: 'mainController'
+            templateUrl: "login.html"
           }
         }
       })
@@ -31,33 +30,66 @@ angular.module('myApp', ['ionic','myApp.mainController'])
         url: '/homePage',
         views: {
           'index.main': {  //所有被index.main管理的路由可以随意跳转
-            templateUrl: "homePage.html",
-            controller: 'mainController'
+            templateUrl: "homePage.html"
           }
         }
       })
-  // .state('punchTheClock',{
-  //     templateUrl: "punchTheClock.html"
-  // })
-  // .state('travelOnBusiness',{
-  //     templateUrl: "travelOnBusiness.html"
-  // })
-  // .state('askedForLeave',{
-  //     templateUrl: "askedForLeave.html"
-  // })
-  // .state('customerManagement',{
-  //     templateUrl:'customerManagement.html'
-  // })
-  // .state('customer_MenDianDetail',{
-  //     templateUrl:'customer_MenDianDetail.html'
-  // })
-  // .state('edit_menDianSummary',{
-  //     templateUrl:'edit_menDianSummary.html'
-  // })
-  // .state('customer_JxsDetail',{
-  //     templateUrl:'customer_JxsDetail.html'
-  // });
-
+      .state("punchTheClock", {
+          url: '/punchTheClock',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "punchTheClock.html"
+              }
+          }
+      })
+      .state("travelOnBusiness", {
+          url: '/travelOnBusiness',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "travelOnBusiness.html"
+              }
+          }
+      })
+      .state("askedForLeave", {
+          url: '/askedForLeave',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "askedForLeave.html"
+              }
+          }
+      })
+      .state("customerManagement", {
+          url: '/customerManagement',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "customerManagement.html"
+              }
+          }
+      })
+      .state("customer_MenDianDetail", {
+          url: '/customer_MenDianDetail',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "customer_MenDianDetail.html"
+              }
+          }
+      })
+      .state("edit_menDianSummary", {
+          url: '/edit_menDianSummary',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "edit_menDianSummary.html"
+              }
+          }
+      })
+      .state("customer_JxsDetail", {
+          url: '/customer_JxsDetail',
+          views: {
+              'index.main': {  //所有被index.main管理的路由可以随意跳转
+                  templateUrl: "customer_JxsDetail.html"
+              }
+          }
+      })
   // .state('login',{
   //     templateUrl: "login.html"
   // })
@@ -87,7 +119,7 @@ angular.module('myApp', ['ionic','myApp.mainController'])
   //     });
   $urlRouterProvider.otherwise('/login');
 });
-// var myUrl = 'http://222.88.22.72:100/userServiceController.do?';
+// var myUrl_47 = 'http://222.88.22.72:100/userServiceController.do?';
 // var myUrl = "http://100.100.1.55:8080/synear/";
    var myUrl = "http://171.8.66.195:8080/synear/";
 // var myUrl = 'http://100.100.1.46:9999/';
--- js/mainController.js
+++ js/mainController.js
@@ -41,8 +41,6 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
     //     // Execute action
     // });
 
-    //设置首页
-    // $state.go('login');
 
     $scope.isMenDianOrJxsGuanLi = 'menDian';
     $scope.userName = 'zf04';
@@ -52,9 +50,8 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
     if(localStorage.ydsw_userDetail){
         $scope.userDetail = JSON.parse(localStorage.ydsw_userDetail);
     }
-    var url = 'http://192.168.16.225:9999/synear/orderReceiptController.do?getWechatConfigureList'+ '&url=' + $location.absUrl() + '&callback=JSON_CALLBACK';
+    var url = 'http://222.88.22.72:100/orderReceiptController.do?getWechatConfigureList'+ '&url=' + window.location.href.split("#")[0] + '&callback=JSON_CALLBACK';
     $http.jsonp(url).success(function (result) {
-        console.log(result);
         if (result) {
             wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -110,7 +107,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
             template: str
         });
         alertPopup.then(function (res) {
-            console.log('点击了确定');
+            // console.log('点击了确定');
         });
     };
     //跳转到新的界面
@@ -119,8 +116,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
         if (str == 'travelOnBusiness'){
             $scope.getLocation_by_wx();
         }
-        // $location.path(str);
-        $state.go(str);
+        $location.path(str);
     };
     //没有时间的日期选择框架
     $scope.timeTouch = function (timeId, preset) {
@@ -168,7 +164,6 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                 for (var i = 0;i < menDianDetaillList.length;i++){
                     $scope.getMenDianOtherSummary(menDianDetaillList[i]);
                 }
-                // $state.go("homePage");
             }else {
                 $scope.promptShow(result.msgDesc);
                 $timeout(function () {
@@ -219,7 +214,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                         }else {
                             $scope.jxsYeWuYuanList = {rows:[{USERNAME:'无数据'}]};
                         }
-                        console.log($scope.jxsYeWuYuanList);
+                        // console.log($scope.jxsYeWuYuanList);
                         break;
                     case 'getProvince':
                         $scope.shengFenList = result;
@@ -229,8 +224,6 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
             }
             if ($scope.lianSuoList && $scope.quDaoList && $scope.buMenList && $scope.xiaoShouDaiBiaoList && $scope.suoShuJxsList && $scope.liHuoYuanList && $scope.jxsYeWuYuanList && $scope.shengFenList){
                 $scope.lodingHide();
-                // $state.go('edit_menDianSummary');
-                // $state.go("homePage");
                 $location.path('/homePage');
             }
         }).error(function () {
@@ -279,11 +272,11 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
             if (result.msgCode == "0001"){
                 $scope.punchTheclockDetail = result.rows;
                 // console.log(result);
-                $state.go('punchTheClock');
+                $location.path('/punchTheClock');
             }else {
-                alert(result.msgDesc);
                 if (result.msgCode == "0002"){
-                    $state.go("login");
+                    alert(result.msgDesc);
+                    $location.path('/login');
                 }
             }
         }).error(function () {
@@ -340,9 +333,9 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                 // console.log(result);
                 $scope.goToThePunchTheClockView();
             }else {
-                alert(result.msgDesc);
                 if (result.msgCode == "0002"){
-                    $state.go("login");
+                    alert(result.msgDesc);
+                    $location.path("/login");
                 }
             }
         }).error(function () {
@@ -352,31 +345,32 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
             }, 500);
         });
     };
+
     //获取位置信息
     $scope.getLocation_by_wx = function (str) {
+        // var baidu_url = "http://api.map.baidu.com/geoconv/v1/?coords=" + '113.6542,34.86024' + "&ak=uCgec5YyeCeFkPLILZLS2guLOraELess&output=json" + '&callback=JSON_CALLBACK';
+        // $http.jsonp(baidu_url).success(function (data) {
+        //     console.log(data);
+        //     longitude = data.result[0].x;
+        //     latitude = data.result[0].y;
+        // }).error(function () {
+        //     $scope.promptShow("网络错误！");
+        //     $timeout(function () {
+        //         $scope.lodingHide();
+        //     }, 500);
+        // });
         wx.getLocation({
             type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
             success: function (res) {
                 var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
                 var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                // var baidu_url = "http://api.map.baidu.com/geoconv/v1/?coords=" + '113.6542,34.86024' + "&ak=uCgec5YyeCeFkPLILZLS2guLOraELess&output=json" + '&callback=JSON_CALLBACK';
-                // $http.jsonp(baidu_url).success(function (data) {
-                //     longitude = data.result[0].x;
-                //     latitude = data.result[0].y;
-                // }).error(function () {
-                //     $scope.promptShow("网络错误！");
-                //     $timeout(function () {
-                //         $scope.lodingHide();
-                //     }, 500);
-                // });
                 // var speed = res.speed; // 速度，以米/每秒计
                 // var accuracy = res.accuracy; // 位置精度
-                // var map = new BMap.Map("allmap");
-                var point = new BMap.Point(parseFloat(longitude) - 0.008774687519, parseFloat(latitude) + 0.00374531687912);
                 // var point = new BMap.Point(parseFloat(longitude), parseFloat(latitude));
                 // var point = new BMap.Point(113.654144,34.860686);
                 $scope.clickLongitude = longitude - 0.008774687519;//存放提交的时候的经度
                 $scope.clickLatitude = latitude + 0.00374531687912;//存放提交的时候的纬度
+                var point = new BMap.Point(parseFloat($scope.clickLongitude), parseFloat($scope.clickLatitude));
                 var gc = new BMap.Geocoder();
                 gc.getLocation(point, function (rs) {
                     var addComp = rs.addressComponents;
@@ -388,9 +382,14 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                     });
 
                 });
+            },
+            cancel: function (res) {
+                alert('用户拒绝授权获取地理位置');
             }
         });
     };
+
+
     //调用微信拍照
     $scope.openCamera = function () {
         if($scope.travelCameraImg){
@@ -455,7 +454,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                 $scope.promptShow("您的请假信息提交成功！");
                 $timeout(function () {
                     $scope.lodingHide();
-                    $state.go('homePage');
+                    $location.path("/homePage");
                 }, 1200);
                 // document.getElementById('leaveSelect').value = "请选择类型";
                 // document.getElementById('dateTimeInput').value = "";
@@ -463,7 +462,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
             }else {
                 alert(result.msgDesc);
                 if (result.msgCode == "0002"){
-                    $state.go("login");
+                    $location.path("/login");
                 }
             }
         }).error(function () {
@@ -477,7 +476,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
     $scope.goToTheCustomerManagement = function () {
         //tab = 2;//2代表默认的是门店 1代表的是经销商
         $scope.customerTab = '2';//初始化请求状态是经销商还是门店
-        $state.go('customerManagement');
+        $location.path("/customerManagement");
         $timeout(function () {
             $scope.getShopOrJxsList();
         }, 300);
@@ -540,7 +539,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                 $scope.isCustomerShowScroll = false;
                 alert(result.msgDesc);
                 if(result.msgCode == '0002'){
-                    $state.go('login');
+                    $location.path('/login');
                 }
             }
         }).error(function () {
@@ -598,7 +597,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                         $scope.isCustomerShowScroll = false;
                         alert(result.msgDesc);
                         if (result.msgCode == "0002") {
-                            $state.go('login');
+                            $location.path('/login');
                         }
                     }
                 }).error(function () {
@@ -635,14 +634,14 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
                 }
             }
             if ($scope.customerTab == '2'){
-                $state.go('customer_MenDianDetail');
+                $location.path('/customer_MenDianDetail');
             }
             if ($scope.customerTab == '1'){
-                $state.go('customer_JxsDetail');
+                $location.path('/customer_JxsDetail');
             }
             if (result.msgCode == '0002'){
                 alert(result.msgDesc);
-                $state.go('login');
+                $location.path('/login');
             }
             if (result.msgCode == '0003'){
                 $scope.showAlert(result.msgDesc);
@@ -837,7 +836,7 @@ angular.module('myApp.mainController',[]).controller('mainController',['$scope',
             }
             if (result.msgCode == '0002'){
                 alert(result.msgDesc);
-                $state.go('login');
+                $location.path('/login');
             }
         }).error(function () {
             $scope.promptShow("网络错误！");
